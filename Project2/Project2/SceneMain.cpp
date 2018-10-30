3a7472870935f1ad98bce5534dd2ac204c03c1a5
HR <h1201819027@hiratagakuen.onmicrosoft.com>
2018-10-30 15:49:57 +0900

    aaaaaa

--- Project2/Project2/SceneMain.cpp
+++ Project2/Project2/SceneMain.cpp
@@ -50,26 +50,6 @@ void CSceneMain::InitScene()
 	CObjItem* i = new CObjItem();//オブジェクト作成
 	Objs::InsertObj(i, OBJ_ITEM, 1);//マネージャに登録
 
-	
-	//BGM・SEの読み込み
-	Audio::LoadAudio(0, L"", SOUND_TYPE::BACK_MUSIC);
-
-	Audio::LoadAudio(0, L"", SOUND_TYPE::EFFECT);
-
-
-	//外部グラフィックを読み込み0番に登録(512×512ピクセル)
-	Draw::LoadImage(L"桃太郎.png", 0,TEX_SIZE_512);
-
-	//外部グラフィックを読み込み1番に登録(512×512ピクセル)
-	Draw::LoadImage(L"アイテム.png", 1, TEX_SIZE_512);
-
-	//主人公オブジェクト作成
-	CObjHero* obj = new CObjHero();//オブジェクト作成
-	Objs::InsertObj(obj, OBJ_HERO, 1);//マネージャに登録
-
-	//アイテムオブジェクト作成
-	CObjItem* i = new CObjItem();//オブジェクト作成
-	Objs::InsertObj(i, OBJ_ITEM, 1);//マネージャに登録
 
 }
 
