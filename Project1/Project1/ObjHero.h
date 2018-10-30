471b88f76eca85d7f6ec8778591ec09832ed0595
suraimu <h1201619018@hiratagakuen.onmicrosoft.com>
2018-10-30 13:49:07 +0900

    アクションの指南書を参考に 主人公の変数をm_xからm_pxに変更

--- Project1/Project1/ObjHero.h
+++ Project1/Project1/ObjHero.h
@@ -10,13 +10,13 @@ class CObjHero :public CObj
 public:
 	CObjHero() {};
 	~CObjHero() {};
-	void Init();//イニシャライズ
-	void Action();//アクション
-	void Draw();//ドロー
+	void Init();	//イニシャライズ
+	void Action();	//アクション
+	void Draw();	//ドロー
 private:
-	//主人公の方向移動用変数
-	float m_x;//x方向移動用変数
-	float m_y;//y方向移動用変数
-	float m_vx;//x方向移動ベクトル用変数
-	float m_vy;//y方向移動ベクトル用変数
+					//主人公の位置と移動用
+	float m_px;		//位置
+	float m_py;
+	float m_vx;		//移動ベクトル用変数
+	float m_vy;
 };
