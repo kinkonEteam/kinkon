a5347f6e4567685a311cf44c3056751485238231
HR <h1201819027@hiratagakuen.onmicrosoft.com>
2018-11-01 15:24:35 +0900

    アイテム　ランダム描画

--- Project2/Project2/ObjItem.cpp
+++ Project2/Project2/ObjItem.cpp
@@ -4,6 +4,8 @@
 #include "GameL\SceneManager.h"
 #include "GameL\DrawFont.h"
 
+#include <stdlib.h>
+#include <time.h>
 #include "ObjItem.h"
 #include "GameHead.h"
 
@@ -14,7 +16,8 @@ using namespace GameL;
 //イニシャライズ
 void CObjItem::Init()
 {
-
+	srand(time(NULL));
+	m_id = ITEM_PEACH + rand() % ITEM_CLUB;
 }
 
 //アクション
@@ -33,16 +36,16 @@ void CObjItem::Draw()
 
 	
 	//切り取り位置の設定
-	src.m_top =0.0f;
-	src.m_left = 0.0f;
-	src.m_right = 32.0f;
-	src.m_bottom = 32.0f;
+	src.m_top =ITEM_RESOURCE_TOP;
+	src.m_left = ITEM_RESOURCE_SIZE*(m_id-ITEM_PEACH)+ITEM_LEFT_OFF_SET;
+	src.m_right = src.m_left + ITEM_SIZE + ITEM_LEFT_OFF_SET;
+	src.m_bottom = src.m_top + ITEM_SIZE - ITEM_TOP_OFF_SET;
 
 	//表示位置の設定
 	dst.m_top = 128.0f ;
 	dst.m_left = 128.0f;
-	dst.m_right = 32.0f + 128.0f;
-	dst.m_bottom = 32.0f + 128.0f;
+	dst.m_right = dst.m_left+ITEM_SIZE;
+	dst.m_bottom = dst.m_top+ITEM_SIZE;
 
 	//描画
 	Draw::Draw(1, &src, &dst, c, 0.0f);
--- Project2/Project2/ObjItem.h
+++ Project2/Project2/ObjItem.h
@@ -5,7 +5,14 @@
 //使用するネームスペース
 using namespace GameL;
 
+#define ITEM_TOP	(111.0f)
+#define ITEM_LEFT	(256.0f)
+#define ITEM_SIZE	(32.0f)
 
+#define ITEM_LEFT_OFF_SET	(2)
+#define ITEM_TOP_OFF_SET	(1)
+#define ITEM_RESOURCE_TOP	(32.0f)
+#define ITEM_RESOURCE_SIZE	(32.0f)
 
 //オブジェクト：キャラクターブロック
 class CObjItem : public CObj
@@ -17,7 +24,8 @@ public:
 	void Action();	//アクション
 	void Draw();	//ドロー
 private:
-	
+	int m_id;	//アイテムID
+
 
 };
 
