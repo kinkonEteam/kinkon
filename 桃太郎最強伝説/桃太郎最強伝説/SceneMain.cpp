fe29eccab8b48f299020e82d5b65b4e3b2b6367e
GA1 <GA1@DEV1-5>
2018-11-05 14:00:28 +0900

    map

---
+++ 桃太郎最強伝説/桃太郎最強伝説/SceneMain.cpp
@@ -0,0 +1,158 @@
+//STLデバッグ機能をOFFにする
+#define _SECURE_SCL (0)
+#define _HAS_ITERATOR_DEBUGGING (0)
+
+//GameLで使用するヘッダー
+#include"GameL\SceneObjManager.h"
+#include"GameL\DrawTexture.h"
+#include"GameL\DrawFont.h"
+#include"GameL\Audio.h"
+#include"GameL\UserData.h"
+
+//使用するネームスペース
+using namespace GameL;
+
+//使用ヘッダー
+#include"SceneMain.h"
+#include"GameHead.h"
+
+//コンストラクタ
+CSceneMain::CSceneMain()
+{
+
+}
+
+//デストラクタ
+CSceneMain::~CSceneMain()
+{
+
+}
+
+//初期化メソッド
+void CSceneMain::InitScene()
+{
+/*	//外部データの読み込み（階層1情報）
+	unique_ptr<wchar_t>p;//ステージ情報ポインター
+	int size;			//ステージ情報の大きさ
+	p = Save::ExternalDataOpen(L"Book1.csv", &size);//外部データ読み込み
+
+	int map[56][56];
+	int count = 1;
+	for (int i = 0; i < 56; i++)
+	{
+		for (int j = 0; j < 56; j++)
+		{
+			int w = 0;
+			swscanf_s(&p.get()[count], L"%d", &w);
+
+			map[i][j] = w;
+			count += 2;
+		}
+	}*/
+	//（階層2情報）
+/*	unique_ptr<wchar_t>p;//ステージ情報ポインター
+	int size;			//ステージ情報の大きさ
+	p = Save::ExternalDataOpen(L"Book2.csv", &size);//外部データ読み込み
+
+	int map[56][56];
+	int count = 1;
+	for (int i = 0; i < 56; i++)
+	{
+		for (int j = 0; j < 56; j++)
+		{
+			int w = 0;
+			swscanf_s(&p.get()[count], L"%d", &w);
+
+			map[i][j] = w;
+			count += 2;
+		}
+	}*/
+/*	//（階層3情報）
+	unique_ptr<wchar_t>p;//ステージ情報ポインター
+	int size;			//ステージ情報の大きさ
+	p = Save::ExternalDataOpen(L"Book3.csv", &size);//外部データ読み込み
+
+	int map[56][56];
+	int count = 1;
+	for (int i = 0; i < 56; i++)
+	{
+		for (int j = 0; j < 56; j++)
+		{
+			int w = 0;
+			swscanf_s(&p.get()[count], L"%d", &w);
+
+			map[i][j] = w;
+			count += 2;
+		}
+	}*/
+	//（階層4情報）
+	unique_ptr<wchar_t>p;//ステージ情報ポインター
+	int size;			//ステージ情報の大きさ
+	p = Save::ExternalDataOpen(L"Book4.csv", &size);//外部データ読み込み
+
+	int map[56][56];
+	int count = 1;
+	for (int i = 0; i < 56; i++)
+	{
+		for (int j = 0; j < 56; j++)
+		{
+			int w = 0;
+			swscanf_s(&p.get()[count], L"%d", &w);
+
+			map[i][j] = w;
+			count += 2;
+		}
+	}
+/*	//（階層5情報）
+	unique_ptr<wchar_t>p;//ステージ情報ポインター
+	int size;			//ステージ情報の大きさ
+	p = Save::ExternalDataOpen(L"Book5.csv", &size);//外部データ読み込み
+
+	int map[56][56];
+	int count = 1;
+	for (int i = 0; i < 56; i++)
+	{
+		for (int j = 0; j < 56; j++)
+		{
+			int w = 0;
+			swscanf_s(&p.get()[count], L"%d", &w);
+
+			map[i][j] = w;
+			count += 2;
+		}
+	}
+	*/
+
+	
+	//Font作成
+	Font::SetStrTex(L"0123456789分秒");
+
+	//グラフィック読み込み
+	Draw::LoadImageW(L"マップチップ.png", 0, TEX_SIZE_141_47);
+
+/*	//階層1オブジェクト作成
+	CObjMap1* objb = new CObjMap1(map);
+	Objs::InsertObj(objb, OBJ_MAP1, 9);*/
+
+/*	//階層2オブジェクト作成
+	CObjMap2* objb = new CObjMap2(map);
+	Objs::InsertObj(objb, OBJ_MAP2, 9);*/
+
+/*	//階層3オブジェクト作成
+	CObjMap3* objb = new CObjMap3(map);
+	Objs::InsertObj(objb, OBJ_MAP2, 9);*/
+
+	//階層4オブジェクト作成
+	CObjMap4* objb = new CObjMap4(map);
+	Objs::InsertObj(objb, OBJ_MAP2, 9);
+
+/*	//階層5オブジェクト作成
+	CObjMap5* objb = new CObjMap5(map);
+	Objs::InsertObj(objb, OBJ_MAP2, 9);*/
+}
+
+//実行中メソッド
+void CSceneMain::Scene()
+{
+
+}
